05182d003ccfed66f86935d4c218333786150046
Shakeel Sharif <muhammad.shakeel@devsinc.com>
2019-12-02 19:22:40 +0500

    A Real App_ The Burger Builder (Basic Version). upto 140.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
   return (
     <Layout>
-      <p>Burger</p>
       <BurgerBuilder/>
     </Layout>
   );
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux'
+import React,{Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component{
+
+  UNSAFE_componentWillUpdate(){
+    console.log('[OrderSummary] will update');
+  }
+// componentWillUpdate(){
+//     console.log('[OrderSummary] will update');
+//   }
+
+  render(){
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
-      return (<li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}</li>);
+      return (<li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}</li>);
     });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with following ingredients</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price:{props.price.toFixed(2)}</strong></p>
-      {/* <p>Continue to checkout?</p> */}
-      <Button btnType={'Danger'} clicked={props.cancelPurchase}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.continuePurchase}>CONTINUE</Button>
-    </Aux>
-  );
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with following ingredients</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price:{this.props.price.toFixed(2)}</strong></p>
+        {/* <p>Continue to checkout?</p> */}
+        <Button btnType={'Danger'} clicked={this.props.cancelPurchase}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.continuePurchase}>CONTINUE</Button>
+      </Aux>
+    );
+  }
 }
 
-export default orderSummary;
+export default OrderSummary;
